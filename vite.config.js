d876e98bcc5e450dc1919fed1789ab8798f1c8b3
damn-devil <damn-devil@users.noreply.github.com>
2026-09-03 14:39:32 +0300

    chore: set base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/bsuir-schedule/',
   server: { port: 5174 },
   build: { target: 'es2017' },
 })
